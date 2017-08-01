405a2071ed33344f7c17a2a3a08eae3efc40f339
Akhilendra Gadde <akhilendragadde17@gmail.com>
2017-08-01 22:58:07 +0530

    Init

--- ciphers.py
+++ ciphers.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
 '''
 @odin
 '''
 from random import randint as r
+_charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def main():
 
@@ -13,57 +15,57 @@ def main():
 		print('4. AutoKey Cipher')
 		print('5. Exit')
 		ch = int(input('Enter your choice : '))
-	
-		if ch == 1:
+
+		if ch is 1:
 			additive()
-		elif ch == 2:
-			ceaser()		
-		elif ch == 3:
+		elif ch is 2:
+			ceaser()
+		elif ch is 3:
 			otpc()
-		elif ch == 4:
+		elif ch is 4:
 			AutoKey()
-		elif ch == 5:
+		elif ch is 5:
 			exit(0)
 		else:
 			print('Invalid Choice')
 			pass
 
 def additive():
-	_charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	print('\n\n\t\t|| Additive Cipher ||\n')
 
+	print('\n\n\t\t|| Additive Cipher ||\n')
 	print('\n1. Encrypt Message')
 	print('2. Decrypt Message')
 	print('3. Return to main')
 	ch = int(input('Enter your choice : '))
-	
-	if ch == 1:
+
+	if ch is 1:
 		_cipher = ''
 		message = str(input('\nEnter message to encrypt : '))
 		key = int(input('Enter key in range[1-26] : '))
-		
+
 		for index,i in enumerate(message.replace(" ","")):
 			char = _charset.find(i.upper()) + key
 			if char > 26:
 				char = char%26
 			_cipher += _charset[char]
-		
+
 		print("\nYour message '{}' is Encrypted as '{}'\n".format(message,_cipher))
 		input('press ENTER to continue\n')
-	elif ch == 2:
+
+	elif ch is 2:
 		_cipher = ''
 		message = str(input('\nEnter message to decrypt : '))
 		key = int(input('Enter key in range[1-26] : '))
-		
+
 		for index,i in enumerate(message.upper().replace(" ","")):
 			char = _charset.find(i) - key
 			if char < 0:
 				char += 26
 			_cipher += _charset[char].lower()
-		
+
 		print("\nYour message '{}' is decrypted as '{}'\n".format(message.upper(),_cipher))
 		input('press ENTER to continue\n')
-	elif ch == 3:
+	elif ch is 3:
 		pass
 	else:
 		print('Invalid Choice')
@@ -71,118 +73,109 @@ def additive():
 
 
 def ceaser():
-	_charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 	print('\n\n\t\t|| Ceaser Cipher ||\n')
 
 	print('\n1. Encrypt Message')
 	print('2. Decrypt Message')
 	print('3. Return to main')
 	ch = int(input('Enter your choice : '))
-	
-	if ch == 1:
+
+	if ch is 1:
 		_cipher = ''
 		message = str(input('\nEnter message to encrypt : '))
 		key = 3
-		
+
 		for index,i in enumerate(message.replace(" ","")):
 			char = _charset.find(i.upper()) + key
 			if char > 26:
 				char = char%26
 			_cipher += _charset[char]
-		
+
 		print("\nYour message '{}' is Encrypted as '{}'\n".format(message,_cipher))
 		input('press ENTER to continue\n')
-	elif ch == 2:
+	elif ch is 2:
 		_cipher = ''
 		message = str(input('\nEnter message to decrypt : '))
 		key = 3
-		
+
 		for index,i in enumerate(message.upper().replace(" ","")):
 			char = _charset.find(i) - key
 			if char < 0:
 				char += 26
 			_cipher += _charset[char].lower()
-		
+
 		print("\nYour message '{}' is decrypted as '{}'\n".format(message.upper(),_cipher))
 		input('press ENTER to continue\n')
-	elif ch == 3:
+	elif ch is 3:
 		pass
 	else:
 		print('Invalid Choice')
 		ceaser()
-	
+
 def otpc():
-	_charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 	print('\n\n\t\t|| One Time Pad Cipher ||\n')
 
 	print('\n1. Encrypt Message')
 	print('2. Return to main')
 	ch = int(input('Enter your choice : '))
-	
-	if ch == 1:
+
+	if ch is 1:
 		_cipher = ''
 		message = str(input('\nEnter message to encrypt : '))
 		key = r(1,26)
-		
+
 		for index,i in enumerate(message.replace(" ","")):
 			char = _charset.find(i.upper()) + key
 			if char > 26:
 				char = char%26
 			_cipher += _charset[char]
-		
+
 		print("\nYour message '{}' is Encrypted as '{}' using key {}\n".format(message,_cipher,key))
 		input('press ENTER to continue\n')
-	elif ch == 2:
+	elif ch is 2:
 		pass
 	else:
 		print('Invalid Choice')
 		otpc()
-	
+
 def AutoKey():
-	_charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 	print('\n\n\t\t|| AutoKey Cipher ||\n')
 
 	print('\n1. Encrypt Message')
 	print('2. Decrypt Message')
 	print('3. Return to main')
 	ch = int(input('Enter your choice : '))
-	
-	if ch == 1:
+
+	if ch is 1:
 		_cipher = ''
 		message = str(input('\nEnter message to encrypt : '))
 		key = int(input('Enter key value in range[1-26] : '))
-		x = 0
-		for index,i in enumerate(message.replace(" ","")):
-			char = _charset.find(i.upper()) + key
-			if char > 26:
-				char = char%26
+		plainText = message.upper().replace(" ","")
+
+		for index,i in enumerate(plainText):
+			if index is 0:
+				char = _charset.find(i) + key
+			else:
+				char = _charset.find(i) + _charset.find(plainText[index - 1])
+				if char > 26:
+					char %= 26
 			_cipher += _charset[char]
-		
+
 		print("\nYour message '{}' is Encrypted as '{}'\n".format(message,_cipher))
 		input('press ENTER to continue\n')
-		
-		
-		
-	elif ch == 2:
-		_cipher = ''
-		message = str(input('\nEnter message to decrypt : '))
-		key = int(input('Enter key in range[1-26] : '))
-		
-		for index,i in enumerate(message.upper().replace(" ","")):
-			char = _charset.find(i) - key
-			if char < 0:
-				char += 26
-			_cipher += _charset[char].lower()
-		
-		print("\nYour message '{}' is decrypted as '{}'\n".format(message.upper(),_cipher))
-		input('press ENTER to continue\n')
-	elif ch == 3:
+
+	elif ch is 2:
+		pass
+
+	elif ch is 3:
 		pass
 	else:
 		print('Invalid Choice')
 		additive()
-	
+
 
 if __name__ == '__main__' :
 	main()
-	
